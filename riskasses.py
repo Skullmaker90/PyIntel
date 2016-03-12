208b6efd9ac6164486792a8ac1937219106b95b5
Anthony Smith <asmith@cari.net>
2016-03-12 14:45:56 -0800

    Simplified cyno_check logic.

--- riskasses.py
+++ riskasses.py
@@ -9,20 +9,17 @@ log = libs.Logging.Log('Risk_log')
 zkb = libs.Zkb.Zkb()
 who = libs.Who.Who()
 
-def check_cyno(pilot):
-  cyno_ids = [21096, 28646]
+def check_cyno(name):
   log.debug('Retriving pilot ID')
-  pilot_id = int(who('character', 'name', pilot)['info']['character_id'])
-  log.debug('Pilot ID: %s' % (pilot_id))
+  pilot_id = int(who('character', 'name', name)['info']['character_id'])
   log.debug('Retrieving Losslist.')
-  rec_loss_list = zkb.query(fmod = ['characterID', pilot_id],
+  pilot_loss_list = zkb.query(fmod = ['characterID', pilot_id],
                             ftypemod = ['losses'],
                             ptlmod = ['limit', 100])
-  log.debug('Losslist retrieved, Length: %s' % (len(rec_loss_list)))
   log.debug('Mapping Km build_objects.')
-  rec_loss_list = map(build_object, rec_loss_list)
+  pilot_loss_list = map(build_object, pilot_loss_list)
   log.debug('Map complete, Returning filter result.')
-  return filter(itemid_comp, rec_loss_list)
+  return filter(itemid_comp, pilot_loss_list)
 
 def build_object(km):
   return libs.Km.Killmail(km)
@@ -30,10 +27,17 @@ def build_object(km):
 def itemid_comp(kmobj):
   iscyno = False
   cyno_ids = [21096, 28648]
-  for item in kmobj.items.highs:
-    if cyno_ids.count(int(item['typeID'])) >= 1:
-      iscyno = True
-  for item in kmobj.items.cargo:
-    if cyno_ids.count(int(item['typeID'])) >= 1:
+  highs, cargo = kmobj.items.highs, kmobj.items.lows
+  for item in (highs + cargo):
+    if item['typeID'] in cyno_ids:
       iscyno = True
   return iscyno
+
+def check_doctorine(name):
+  pilot_id = int(who('character', 'name', pilot)['info']['character_id'])
+
+  def compare_items(kmobj):
+    key_match = ['highs', 'mids', 'lows']
+    for k, v in kmobj.items():
+      if key_match.count(k) == 1:
+        pass
