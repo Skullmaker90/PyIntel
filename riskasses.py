67e316f83b059de88a64a165389a59bd300b023f
Anthony Smith <asmith@cari.net>
2016-03-12 15:33:44 -0800

    Dict'enized the cyno_id's

--- riskasses.py
+++ riskasses.py
@@ -9,13 +9,13 @@ log = libs.Logging.Log('Risk_log')
 zkb = libs.Zkb.Zkb()
 who = libs.Who.Who()
 
-def check_cyno(name):
+def check_cyno(name, limit=100):
   log.debug('Retriving pilot ID')
   pilot_id = int(who('character', 'name', name)['info']['character_id'])
   log.debug('Retrieving Losslist.')
   pilot_loss_list = zkb.query(fmod = ['characterID', pilot_id],
                             ftypemod = ['losses'],
-                            ptlmod = ['limit', 100])
+                            ptlmod = ['limit', limit])
   log.debug('Mapping Km build_objects.')
   pilot_loss_list = map(build_object, pilot_loss_list)
   log.debug('Map complete, Returning filter result.')
@@ -25,13 +25,15 @@ def build_object(km):
   return libs.Km.Killmail(km)
 
 def itemid_comp(kmobj):
-  iscyno = False
-  cyno_ids = [21096, 28648]
-  highs, cargo = kmobj.items.highs, kmobj.items.lows
+  is_cyno = False
+  cyno_ids = {'Cynosural Field Generator': 21096,
+              'Covert Cynosural Field Generator': 28648}
+  highs = kmobj.items.highs
+  cargo = kmobj.items.cargo
   for item in (highs + cargo):
-    if item['typeID'] in cyno_ids:
-      iscyno = True
-  return iscyno
+    if item['typeID'] in cyno_ids.values():
+      is_cyno = True
+  return is_cyno
 
 def check_doctorine(name):
   pilot_id = int(who('character', 'name', pilot)['info']['character_id'])
