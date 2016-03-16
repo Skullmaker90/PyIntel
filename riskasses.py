539af6b9d513d5065ac92668eea4d609670ebab1
Anthony Smith <asmith@cari.net>
2016-03-16 05:17:52 -0700

    Moved around some stuff, created a populate method for pilot class.

--- riskasses.py
+++ riskasses.py
@@ -25,20 +25,34 @@ class Pilot(object):
   def pop(self, key):
     return self.data.pop(key)
 
-def check_cyno(pilot, limit=100):
-  log.info('Storing pilot name/limit: %s/%s' % (pilot.get('name'), limit))
-  log.info('Retriving pilot ID')
-  pid = int(who('character', 'name', pilot.get('name'))['info']['character_id'])
-  pilot.put('id', pid)
-  log.info('Retrieving Losslist.')
-  loss_list = zkb.query(fmod = ['characterID', pilot.get('id')],
-                            ftypemod = ['losses'],
-                            ptlmod = ['limit', limit])
-  log.info('Mapping Km build_objects.')
-  loss_obj_list = map(build_object, loss_list)
-  pilot.put('losses', loss_obj_list)
-  log.info('Map complete, Returning filter result.')
-  return filter(itemid_comp, loss_obj_list)
+def get_items(pilotobj, name, limit=100):
+  log.info('Pilot name/limit:  %s/%s' % (name, limit))
+  pilotobj.put('name', name)
+  log.info('Retrieving ID.')
+  pilot_id = int(who('character', 'name', pilotobj.get('name'))['info']['characterID'])
+  log.info('%s ID: %s' % (name, pilot_id))
+  pilotobj.put('id', pilot_id)
+  log.info('Retriving Losses.')
+  pilot_losses = get_ent_losses(pilotobj, 'characterID', pilot_id, limit)
+  pilotobj.put('losses', pilot_losses)
+  pilot_corp_losses = get_ent_losses(pilotobj, 'corporationID', pilot_losses[0]\
+                                                                ['victim']\
+                                                                ['corporationID'])
+  pilotobj.put('corp_losses', pilot_corp_losses)
+  return pilotobj
+
+def get_ent_losses(pilotobj, idtype, _id, limit=100):
+  log.info('Retriving killmails: %s/%s' % (pilotobj.get('name'), limit))
+  losses = zkb.query(fmod = [idtype, _id],
+                      ftypemod = ['losses'],
+                      ptlmod = ['limit', limit])
+  log.info('Objectifing losses (Sorry).')
+  losses = map(build_obj, losses)
+  return losses
+
+def check_cyno(pilotobj):
+  losses = pilotobj.get('losses')
+  return filter(itemid_comp, losses)
 
 def build_object(km):
   return libs.Km.Killmail(km)
@@ -54,10 +68,23 @@ def itemid_comp(kmobj):
       is_cyno = True
   return is_cyno
 
-def check_doctorine():
-  pass
+def check_doctorine(pilotobj, limit=100):
+  doc_list = []
+  losses = pilotobj.get('losses')
+  corp_losses = pilotobj.get('corp_losses')
+  pairs = zip(losses, corp_losses)
+  for item in pairs:
+    kms = []
+    _id = item['victim']['shipTypeID']
+    sim_km = next((ent for ent in pairs if ent['id'] == _id), None)
+    if sim_km:
+      for cls_method in item.items.__dict__:
+        pass
+    
 
 def main(name):
-  pilot = Pilot()
-  pilot.put('name', name)
-  return pilot
+  if isinstance(name, (str, unicode)):
+    pilot = Pilot()
+    pilot.put('name', name)
+    return pilot
+  raise TypeError("Passed '%s' not str" % (name))
